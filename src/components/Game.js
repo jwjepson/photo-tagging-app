422445eed8943153322877fa2d4b86f9158de1e6
Jake Jepson <jwjepson1@gmail.com>
2023-03-06 17:26:36 -0500

    get coords as percentage when user clicks on image

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "../styles/game.css";
+import Level1 from "../images/cropped.jpg";
 
 const Game = () => {
 
     const getCoords = (e) => {
-        let x = e.pageX;
-        let y = e.pageY;
-        console.log(x, y);
+        let x = (e.nativeEvent.offsetX / e.target.clientWidth).toFixed(2);
+        let y = (e.nativeEvent.offsetY / e.target.clientHeight).toFixed(2);
     }
 
     return (
-        <div onClick={getCoords} className="main"></div>
+        <div className="game-container">
+            <div className="timer">Timer: 5:03</div>
+            <div className="image-container">
+                <img onClick={getCoords} src={Level1} className="game-image"></img>
+            </div>
+        </div>
     )
 }
 
