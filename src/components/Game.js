f3dd27c80262ecb27c58f95ac2b46a8e37569ccf
Jake Jepson <jwjepson1@gmail.com>
2023-03-17 18:32:19 -0400

    grab clicked coords and check database if correctly clicked

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/game.css";
 import Level1 from "../images/cropped.jpg";
+import { click } from "@testing-library/user-event/dist/click";
 
-const Game = () => {
+const Game = (props) => {
 
-    const getCoords = (e) => {
+    const handleClick = (e) => {
         let x = (e.nativeEvent.offsetX / e.target.clientWidth).toFixed(2);
         let y = (e.nativeEvent.offsetY / e.target.clientHeight).toFixed(2);
+        let selectedCoords = `${x}, ${y}`;
+        console.log(selectedCoords);
+        checkAnswer(selectedCoords);
+    }
+
+    const checkAnswer = async (selectedCoords) => {
+        let data = await props.getData();
+        const coords = data[0]["x-coord"];
+        
+        if (coords.includes(selectedCoords)) {
+            alert("Yes");
+        } else {
+            console.log("No");
+        }
     }
 
     return (
+        <>
         <div className="game-container">
-            <div className="timer">Timer: 5:03</div>
+            <div className="timer">Timer: 0:00</div>
             <div className="image-container">
-                <img onClick={getCoords} src={Level1} className="game-image"></img>
+                <img onClick={handleClick} alt="Game"src={Level1} className="game-image"></img>
             </div>
         </div>
+        </>
     )
 }
 
