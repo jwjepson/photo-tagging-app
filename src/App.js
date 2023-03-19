6315179bc5aac2c43f9f8ab348a524d3f5400099
Jake Jepson <jwjepson1@gmail.com>
2023-03-19 19:53:08 -0400

    import addDoc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore/lite";
 import React, {useEffect, useState} from "react";
 import Header from './components/Header';
 import Game from './components/Game';
