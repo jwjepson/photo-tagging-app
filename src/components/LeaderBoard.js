b33f3517e49e9c83c0cb14cb49abfcffebbc567c
Jake Jepson <jwjepson1@gmail.com>
2023-03-19 19:19:05 -0400

    grab leaderboard data from database and render

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,18 +1,39 @@
-import React from "react";
+import { getDocs } from "firebase/firestore/lite";
+import React, { useEffect, useState } from "react";
 import "../styles/leaderboard.css";
+import { collection } from "firebase/firestore/lite";
 
-const LeaderBoard = () => {
+const LeaderBoard = (props) => {
+    const [leaderBoard, setLeaderBoard] = useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            const leaderboardCol = collection(props.db, "leaderboard");
+            const leaderboardSnapshot = await getDocs(leaderboardCol);
+            const leaderboard = leaderboardSnapshot.docs.map(doc => doc.data());
+            console.log(leaderboard);
+            setLeaderBoard(leaderboard);
+        }
+        getData();
+    }, []);
+
     return (
         <div className="leaderboard-container">
             <table className="leaderboard-table">
-                <tr>
-                    <th>Name</th>
-                    <th>Best-Time</th>
-                </tr>
-                <tr>
-                    <td>Test User</td>
-                    <td>Test Score</td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Best-Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {leaderBoard.map(user => (
+                        <tr key={user.index}>
+                            <td>{user.name}</td>
+                            <td>{user.time}</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </div>
     )
