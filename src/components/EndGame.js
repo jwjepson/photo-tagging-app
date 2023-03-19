2cc260135ea62850342faf481dd50a55396c13ec
Jake Jepson <jwjepson1@gmail.com>
2023-03-19 18:21:04 -0400

    create endgame component popup that allows users to save score to leaderboard

---
+++ src/components/EndGame.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const EndGame = (props) => {
+    return (
+        <div className="endGameModal">
+            <div className="info">
+                <div className="gameOverMessage">You found Waldo!</div>
+                <div className="timeFinished">Your Time: {props.time} seconds</div>
+            </div>
+            <div className="addToLeaderBoard">
+                <div>Enter your name to save your time to the LeaderBoard</div>
+                <form>
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" id="name"></input>
+                    <button className="addToLeaderBoard-button"type="submit">Add to Leaderboard</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default EndGame;
