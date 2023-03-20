6c45ff1978e10f81e414cf972701efd53b478b40
Jake Jepson <jwjepson1@gmail.com>
2023-03-20 13:37:05 -0400

    change list items text into icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "../styles/header.css";
 import { Link } from "react-router-dom";
+import leaderBoardIcon from "../icons/leaderboard.svg";
+import homeIcon from "../icons/home.svg";
 
 const Header = () => {
     return (
         <div className="header">
-            <h1 className="logo">Logo</h1>
+            <h1 className="logo"><span>WHERE'S</span> <span>WALDO?</span></h1>
             <div className="nav-bar">
                 <ul>
                     <Link to="/">
-                        <li>Home</li>
+                        <li><img className="home-icon" alt="Home"src={homeIcon}></img></li>
                     </Link>
                     <Link to="/leaderboard">
-                        <li>Leaderboard</li>
+                        <li><img className="leaderboard-icon" alt="Leaderboard" src={leaderBoardIcon}></img></li>
                     </Link>
                 </ul>
             </div>
