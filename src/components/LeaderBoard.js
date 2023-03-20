5c1701a11aae54bd8ea78894be6e18ca8ef4a7b0
Jake Jepson <jwjepson1@gmail.com>
2023-03-20 18:48:38 -0400

    create a loading spinner for leaderboard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -2,9 +2,11 @@ import { getDocs } from "firebase/firestore/lite";
 import React, { useEffect, useState } from "react";
 import "../styles/leaderboard.css";
 import { collection, query, orderBy } from "firebase/firestore/lite";
+import { BeatLoader } from "react-spinners";
 
 const LeaderBoard = (props) => {
     const [leaderBoard, setLeaderBoard] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const getData = async () => {
@@ -14,32 +16,39 @@ const LeaderBoard = (props) => {
             const leaderboard = leaderboardSnapshot.docs.map(doc => doc.data());
             console.log(leaderboard);
             setLeaderBoard(leaderboard);
+            setTimeout(() => {
+                setLoading(false);
+            }, 300);
         }
         getData();
     }, []);
 
     return (
         <div className="leaderboard-container">
-            <table className="leaderboard-table">
-                <thead>
-                    <tr>
-                        <th>Place</th>
-                        <th>Name</th>
-                        <th>Best-Time</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {leaderBoard.map((user, index) => (
-                        <tr key={index}>
-                            <td>{index + 1}</td>
-                            <td>{user.name}</td>
-                            <td>
-                            {("0" + Math.floor((user.time / 60) % 60)).slice(-2) + ":" + ("0" + Math.floor(user.time % 60)).slice(-2)}
-                            </td>
+            {loading ? (
+                <BeatLoader color="#00A5E3" className="loading" loading={loading}/>
+            ) : (
+                <table className="leaderboard-table">
+                    <thead>
+                        <tr>
+                            <th>Place</th>
+                            <th>Name</th>
+                            <th>Best-Time</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {leaderBoard.map((user, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{user.name}</td>
+                                <td>
+                                {("0" + Math.floor((user.time / 60) % 60)).slice(-2) + ":" + ("0" + Math.floor(user.time % 60)).slice(-2)}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     )
 };
