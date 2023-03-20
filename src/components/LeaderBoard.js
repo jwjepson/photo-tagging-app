7fa4ed075006752d7f8acda18462f45eace6ec5b
Jake Jepson <jwjepson1@gmail.com>
2023-03-20 15:25:52 -0400

    order data by time, display place in leaderboard, and format time

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,7 +1,7 @@
 import { getDocs } from "firebase/firestore/lite";
 import React, { useEffect, useState } from "react";
 import "../styles/leaderboard.css";
-import { collection } from "firebase/firestore/lite";
+import { collection, query, orderBy } from "firebase/firestore/lite";
 
 const LeaderBoard = (props) => {
     const [leaderBoard, setLeaderBoard] = useState([]);
@@ -9,7 +9,8 @@ const LeaderBoard = (props) => {
     useEffect(() => {
         const getData = async () => {
             const leaderboardCol = collection(props.db, "leaderboard");
-            const leaderboardSnapshot = await getDocs(leaderboardCol);
+            const q = query(leaderboardCol, orderBy("time"));
+            const leaderboardSnapshot = await getDocs(q);
             const leaderboard = leaderboardSnapshot.docs.map(doc => doc.data());
             console.log(leaderboard);
             setLeaderBoard(leaderboard);
@@ -22,15 +23,19 @@ const LeaderBoard = (props) => {
             <table className="leaderboard-table">
                 <thead>
                     <tr>
+                        <th>Place</th>
                         <th>Name</th>
                         <th>Best-Time</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {leaderBoard.map(user => (
-                        <tr key={user.index}>
+                    {leaderBoard.map((user, index) => (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
                             <td>{user.name}</td>
-                            <td>{user.time} seconds</td>
+                            <td>
+                            {("0" + Math.floor((user.time / 60) % 60)).slice(-2) + ":" + ("0" + Math.floor(user.time % 60)).slice(-2)}
+                            </td>
                         </tr>
                     ))}
                 </tbody>
