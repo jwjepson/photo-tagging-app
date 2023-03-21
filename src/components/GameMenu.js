915325821d7ba5f2352944c2761593beefd8c956
Jake Jepson <jwjepson1@gmail.com>
2023-03-21 12:56:23 -0400

    remove unnecessary imports

--- src/components/GameMenu.js
+++ src/components/GameMenu.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import "../styles/gamemenu.css";
 import waldoImage from "../images/waldo-image.png";
 import { Link } from "react-router-dom";
