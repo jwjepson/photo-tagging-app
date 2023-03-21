202aaccfd58a41f032decdd9524939d24382d7bb
Jake Jepson <jwjepson1@gmail.com>
2023-03-20 20:00:23 -0400

    if showAlert is true display alert message for 3 seconds, then hide and reset alert to default

--- src/components/GameMenu.js
+++ src/components/GameMenu.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../styles/gamemenu.css";
 import waldoImage from "../images/waldo-image.png";
 import { Link } from "react-router-dom";
 
 const GameMenu = (props) => {
+    useEffect(() => {
+        let timeout;
+        if (props.showAlert) {
+            timeout = setTimeout(() => {
+                props.setShowAlert(false);
+                props.setAlertMessage("");
+            }, 3000);
+        }
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, [props.showAlert]);
+
     return (
-        <div className="game-menu">
-            <img className="waldo-image menu"alt="Waldo" src={waldoImage}></img>
-            <div className="menu-info">
-                <h2>Wheres Waldo?</h2>
-                <p>Try and find Waldo as fast as you can</p>
-                <Link to="/play">
-                    <button className="start-game-button" onClick={props.startGame} type="button">Start Game</button>
-                </Link>
+        <>
+            {props.showAlert && (
+                <div className="alert">{props.alertMessage}</div>
+            )}
+            <div className="game-menu">
+                <img className="waldo-image menu" alt="Waldo" src={waldoImage}></img>
+                <div className="menu-info">
+                    <h2>Wheres Waldo?</h2>
+                    <p>Try and find Waldo as fast as you can</p>
+                    <Link to="/play">
+                        <button className="start-game-button" onClick={props.startGame} type="button">Start Game</button>
+                    </Link>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
